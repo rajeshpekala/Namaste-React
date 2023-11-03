14ffab8e7864c1074d50c8e59b7e1278c8d7e265
rajeshpekala <pekalarajesh001@gmail.com>
2023-11-03 11:47:48 +0530

    episode02 without readmechanges

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
-
+import React from "react"
+import  ReactDOM from "react-dom/client";
 
 /*
 const heading = React.createElement(
@@ -15,8 +16,8 @@ child div indulo h1 tag pettu
 */
 const parent = React.createElement("div",{id : "parent"},
 React.createElement("div",{id : "child"},[
-React.createElement("h1",{},"Iam h1 tag")
-,React.createElement("h2",{},"Iam h2 tag")]
+React.createElement("h1",{},"Hello Rajesh")
+,React.createElement("h2",{},"Random Things h2 tag")]
 )
 
 )
